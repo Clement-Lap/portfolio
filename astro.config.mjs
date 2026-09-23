86939b89b0198bae58d982a886d85637d43a1972
Clement LAPORTE <clem@etik.com>
2026-09-23 15:37:04 +0200

    config: added `robots.txt` to the sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,51 @@
 import { defineConfig, fontProviders } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import { getEnv } from "astro/env/runtime";
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    locales: ["fr", "en"],
-    defaultLocale: "en",
-    routing: {
-      prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
-    },
+	i18n: {
+		locales: ["fr", "en"],
+		defaultLocale: "en",
+		routing: {
+			prefixDefaultLocale: true,
+			redirectToDefaultLocale: true,
+		},
 	},
-	site: "https://clems.dev",
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: "Kufam",
-      cssVariable: "--font-kufam",
-      fallbacks: ["sans-serif"],
-      weights: [600, 700],
-    },
-    {
-      provider: fontProviders.fontsource(),
-      name: "Lexend",
-      cssVariable: "--font-lexend",
-      fallbacks: ["sans-serif"],
-      weights: [400, 500],
-    },
-    {
-      provider: fontProviders.fontsource(),
-      name: "Akaya Telivigala",
-      cssVariable: "--font-akaya",
-      fallbacks: ["serif"],
-    },
+	site: getEnv("SITE_URL"),
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Kufam",
+			cssVariable: "--font-kufam",
+			fallbacks: ["sans-serif"],
+			weights: [600, 700],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Lexend",
+			cssVariable: "--font-lexend",
+			fallbacks: ["sans-serif"],
+			weights: [400, 500],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Akaya Telivigala",
+			cssVariable: "--font-akaya",
+			fallbacks: ["serif"],
+		},
 	],
 	vite: {
 		build: {
 			cssMinify: "esbuild", // "lightningcss" not compatible with deno sadly
-		}
+		},
 	},
 
-  integrations: [sitemap()],
+	integrations: [
+		sitemap({
+			i18n: { defaultLocale: "en", locales: ["fr", "en"] },
+			customPages: [`${getEnv("SITE_URL")}/robots.txt`],
+		}),
+	],
 });
